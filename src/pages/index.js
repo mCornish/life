ab4974e357fafe6d28f77ec5fd01bc057c6aee3f
Mike Cornish <mcornish@reality.ai>
2020-07-28 19:09:29 -0400

    Updates layout, title, and favicon.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,16 @@
 import React from "react"
 import App from '../components/App';
+import Helmet from 'react-helmet';
 
 export default function Home() {
-  return <App/>
+  return (
+    <div>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Life | Mike Cornish</title>
+          <link rel="canonical" href="http://life.mikecornish.me" />
+        </Helmet>
+      <App/>
+    </div>
+  )
 }
